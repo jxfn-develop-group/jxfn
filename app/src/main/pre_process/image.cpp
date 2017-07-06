b43d95581a912978cfa951d34ddfdb446d2cfa32
laxect <laxect39@gmail.com>
2017-07-06 20:02:43 +0800

    standard

--- app/src/main/pre_process/image.cpp
+++ app/src/main/pre_process/image.cpp
@@ -1,3 +1,5 @@
+#include <iostream>
+
 #include "image.hpp"
 
 
@@ -33,8 +35,8 @@ std::map<int, int> Image::imageHist()
 {
     std::map<int, int> res;
     // Image::iterator is a std::vector<std::vector<int>>::iterator.
-    for (Image::iterator i = this->begin(); i != this->end(); i++) {
-        for (std::vector<int>::iterator j = i->begin(); j != i->end(); j++) {
+    for (auto i = this->begin(); i != this->end(); i++) {
+        for (auto j = i->begin(); j != i->end(); j++) {
             if (res.find(*j) != res.end()) {
                 res[*j] += 1;
             }
@@ -49,10 +51,11 @@ std::map<int, int> Image::imageHist()
 
 
 // Invert the image. Be ware of that this func doesn't return a Image.
+// also do the 2-value work.
 void Image::imageInvert(int threshold)
 {
-    for (Image::iterator i = this->begin(); i != this->end(); i++) {
-        for (std::vector<int>::iterator j = i->begin(); j != i->end(); j++) {
+    for (auto i = this->begin(); i != this->end(); i++) {
+        for (auto j = i->begin(); j != i->end(); j++) {
             if (*j > threshold) {
                 *j = 0;
             }
@@ -67,8 +70,8 @@ void Image::imageInvert(int threshold)
 // change the image to a 2-value image.
 void Image::imageTwoValue(int threshold)
 {
-    for (Image::iterator i = this->begin(); i != this->end(); i++) {
-        for (std::vector<int>::iterator j = i->begin(); j != i->end(); j++) {
+    for (auto i = this->begin(); i != this->end(); i++) {
+        for (auto j = i->begin(); j != i->end(); j++) {
             if (*j > threshold) {
                 *j = 255;
             }
@@ -76,5 +79,35 @@ void Image::imageTwoValue(int threshold)
                 *j = 0;
             }
         }
+    }
+}
+
+
+// use the hist table to smart choose a threshold.
+// automate do the Invert when needed.
+// the stardand image means a black background (0) and white text (255)
+void Image::imageStandard()
+{
+    std::map<int, int> hist = this->imageHist();
+
+    auto maxItem = hist.begin();
+    auto minItem = hist.begin();
+    for (auto i = hist.begin(); i != hist.end(); i++) {
+        if (i->second > maxItem->second) {
+            maxItem = i;
+        }
+        if (i->second < minItem->second) {
+            minItem = i;
+        }
+    }
+
+    std::cout << maxItem->first << ' ' << minItem->first << '\n';
+
+    // if the background is white (255), then invert it.
+    if (maxItem->first > minItem->first) {
+        this->imageInvert(minItem->second);
+    }
+    else {
+        this->imageTwoValue(minItem->second);
     }
 }
--- app/src/main/pre_process/test.cpp
+++ app/src/main/pre_process/test.cpp
@@ -3,7 +3,7 @@
 
 
 int main(int argc, char const *argv[]) {
-    int array[4] = {1, 147, 128, 255};
+    int array[4] = {1, 147, 255, 255};
     Image img(2, 2, array);
 
     for (size_t i = 0; i < 2; i++) {
@@ -12,20 +12,13 @@ int main(int argc, char const *argv[]) {
         }
     }
 
-    std::map<int, int> res = img.imageHist();
+    auto res = img.imageHist();
 
     for (std::map<int, int>::iterator it = res.begin(); it != res.end(); it++) {
         std::cout << it->first << ' ' << it->second << '\n';
     }
 
-    img.imageTwoValue(128);
-    for (size_t i = 0; i < 2; i++) {
-        for (size_t j = 0; j < 2; j++) {
-            std::cout << img[i][j] << '\n';
-        }
-    }
-
-    img.imageInvert(128);
+    img.imageStandard();
     for (size_t i = 0; i < 2; i++) {
         for (size_t j = 0; j < 2; j++) {
             std::cout << img[i][j] << '\n';
